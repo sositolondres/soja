eaabfb5ea7d39f31654b6f84dd8c8f6a07f089f8
sositolondres <76189890+sositolondres@users.noreply.github.com>
2024-06-28 19:42:17 -0300

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import axios from "axios";
+import cheerio from "cheerio";
+
+const app = express();
+
+app.get("/api/soja", async (req, res) => {
+  try {
+    // URL de la página web que contiene el precio de la soja
+    const url = "https://www.cac.bcr.com.ar/es/precios-de-pizarra"; // Reemplaza con la URL correcta
+
+    // Realiza la solicitud HTTP a la página web
+    const { data } = await axios.get(url);
+
+    // Carga el HTML en Cheerio
+    const $ = cheerio.load(data);
+
+    // Selecciona el elemento y obtiene su texto
+    let price = $("div.boards-container > div.board.board-soja > div > div.price").text();
+
+    // Elimina todo lo que sigue después de una coma
+
+    if (price.includes(",")) {
+      price = price.split(",")[0];
+    } else {
+      price = $("div.boards-container > div.board.board-girasol > div > div.price-sc").text();
+      price = price.split("(Estimativo) ")[1];
+      price = price.split(",")[0];
+    }
+
+    // Responde con el precio
+    res.json({ price: price.trim() });
+  } catch (error) {
+    console.error("Error scraping the website:", error);
+    res.status(500).json({ error: "Failed to fetch the price of soja" });
+  }
+});
+
+module.exports = app;
